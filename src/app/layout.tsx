e0bbceb23440043873a9475ca282ace67e893d97
vighnesh <59088-VighneshSadagopal@users.noreply.drupalcode.org>
2025-09-19 17:17:46 +0530

    Added SEO related changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Vighnesh Sadagopal",
-  description: "Passionate about creating beautiful, functional web experiences. Let's connect and build something amazing together!",
+  description:
+    "Passionate about creating beautiful, functional web experiences. Let's connect and build something amazing together!",
   icons: {
     icon: "/joker.svg", // can also use .png or .svg
   },
@@ -14,13 +15,31 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Vighnesh Sadagopal",
+    url: "https://vighneshsadagopal.com",
+    sameAs: [
+      "https://www.linkedin.com/in/vighnesh-sadagopal/",
+      "https://github.com/VighneshSadagopal",
+    ],
+    jobTitle: "Full Stack Engineer",
+    worksFor: {
+      "@type": "Organization",
+      name: "QED42",
+    },
+  };
+
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </head>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
